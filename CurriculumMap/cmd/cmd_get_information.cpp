20ddaf4e30357291392002937805046d17bf44e9
npencil <90024054+npencil@users.noreply.github.com>
2022-01-24 11:45:41 +0800

    improved

--- CurriculumMap/cmd/cmd_get_information.cpp
+++ CurriculumMap/cmd/cmd_get_information.cpp
@@ -21,7 +21,7 @@ int read_csv(QString Dir)
 {
     string filename = getcwd(NULL,0);
 #ifdef Q_OS_MACOS
-    filename=(Dir+"/../../../courses.csv").toStdString();
+    filename=(Dir+"/../../courses.csv").toStdString();
 //    Dir.toStdWString()
 #else
     filename+="/courses.csv";
@@ -48,21 +48,35 @@ int read_csv(QString Dir)
     getline(ifs, buffer, ',');
     ifs >> all_course_number;
     // 1st line rest
-    getline(ifs, buffer, '@');
     getline(ifs, buffer);
+
     // 2nd line
-    getline(ifs, buffer, '@');
     getline(ifs, buffer);
 
     // courses line
     for (int row = 0; row < all_course_number; ++ row)
     {
         // 9 commas, 10 columns
+        /*
         getline(ifs, buffer, '@');
         while (buffer.back() == '\r')
             buffer.pop_back();
+        */
         //cout << "row : " << row << endl;
         buffer.append(",");
+        for (int col = 0; col < 11; ++ col)
+        {
+            getline(ifs, buffer, ',');
+            for (int i = 0; i < buffer.length(); ++ i)
+            {
+                if (buffer[i] == '@') buffer[i] = ',';
+                else if (buffer[i] == '$') buffer[i] = '\"';
+                else if (buffer[i] == '#') buffer[i] = '\'';
+            }
+            input[row][col] = QString::fromStdString(buffer);
+        }
+        getline(ifs, buffer);
+        /*
         for (int col = 0, i = 0; col < 12; ++ col)
         {
 //            cout << "col : " << col << endl;
@@ -77,6 +91,7 @@ int read_csv(QString Dir)
             if (delim == '"') ++ i;
         }
         getline(ifs, buffer);
+        */
     }
     ifs.close();
     
