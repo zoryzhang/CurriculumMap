beafe4954f975ba7593734565a4ba4bd5f231d3a
npencil <90024054+npencil@users.noreply.github.com>
2022-01-24 11:45:13 +0800

    improved

--- get-information/get-information.py
+++ get-information/get-information.py
@@ -40,7 +40,24 @@ def is_number(s):
 
     return False
 
-
+def get_content(content):
+    # content is a string
+    # badstring
+    content = content.replace("&amp;", "&")
+    #content = content.replace("&quot;", "$")
+    #content = content.replace("&apos;", "#")
+    content = content.replace("“","$")
+    content = content.replace("”","$")
+    content = content.replace("\"", "$")
+    content = content.replace("‘","#")
+    content = content.replace("’","#")
+    content = content.replace("\'","#")
+    content = content.replace("&lt;", "<")
+    content = content.replace("&gt;", ">")
+    # csv specific
+    content = content.replace(",", "@")
+    content = content.strip('\xa0')
+    return content
 
 def main():
     print('this message is from main function')
@@ -66,13 +83,14 @@ def main():
 
     '''
 
-    real_list = ["ACCT","AESF","AIAA","AMAT","BIBU","BIEN","BSBE","BTEC","CBME","CENG","CHEM","CHMS","CIEM","CIVL","CMAA","COMP","CPEG","CSIT","DASC","DBAP","DSAA","DSCT","ECON","EEMT","EESM","ELEC","EMBA","ENEG","ENGG","ENTR","ENVR","ENVS","EOAS","EVNG","EVSM","FINA","FTEC","FUNH","GBUS","GFIN","GNED","HART","HLTH","HMMA","HUMA","IBTM","IDPO","IEDA","IIMP","IMBA","INFH","INTR","IOTA","IPEN","ISDN","ISOM","JEVE","LABU","LANG","LIFS","MAED","MAFS","MARK","MASS","MATH","MECH","MESF","MFIT","MGCS","MGMT","MICS","MILE","MIMT","MSBD","MSDM","MTLE","NANO","OCES","PDEV","PHYS","PPOL","RMBI","ROAS","SBMT","SCIE","SEEN","SHSS","SMMG","SOCH","SOSC","SUST","SYSH","TEMG","UGOD","UROP","WBBA"]
+    course_abbr = ["ACCT","AESF","AIAA","AMAT","BIBU","BIEN","BSBE","BTEC","CBME","CENG","CHEM","CHMS","CIEM","CIVL","CMAA","COMP","CPEG","CSIT","DASC","DBAP","DSAA","DSCT","ECON","EEMT","EESM","ELEC","EMBA","ENEG","ENGG","ENTR","ENVR","ENVS","EOAS","EVNG","EVSM","FINA","FTEC","FUNH","GBUS","GFIN","GNED","HART","HLTH","HMMA","HUMA","IBTM","IDPO","IEDA","IIMP","IMBA","INFH","INTR","IOTA","IPEN","ISDN","ISOM","JEVE","LABU","LANG","LIFS","MAED","MAFS","MARK","MASS","MATH","MECH","MESF","MFIT","MGCS","MGMT","MICS","MILE","MIMT","MSBD","MSDM","MTLE","NANO","OCES","PDEV","PHYS","PPOL","RMBI","ROAS","SBMT","SCIE","SEEN","SHSS","SMMG","SOCH","SOSC","SUST","SYSH","TEMG","UGOD","UROP","WBBA"]
+    #course_abbr = ["ACCT"]
     base_url = ['https://prog-crs.ust.hk/ugcourse/2021-22/']
 
     
-    for it_A in real_list:
+    for abbr in course_abbr:
         #use soup to find "li" Label and specified classes
-        real_url = base_url[0] + it_A
+        real_url = base_url[0] + abbr
         html = urllib.request.urlopen(real_url).read()
         soup = BeautifulSoup(html, "lxml")
         header_find = soup.find_all("li", {"class": "crse accordion-item"})
@@ -82,92 +100,83 @@ def main():
             row = [] # Operation row
             st = reg.sub('$', str(child))  # Regular Expression Replace
             st = st.strip()
-            t_list = st.split('$')
-            t_list = list(filter(None, t_list))
-            #print(t_list)
-            row.append(t_list[0]) # Code
-            row.append(t_list[1]) # Name
-            row.append(t_list[2]) # Credits
+            #print(st)
+            entries = st.split('$')
+            entries = list(filter(None, entries))
+            #print(entries)
+            row.append(get_content(entries[0])) # Code
+            row.append(get_content(entries[1])) # Name
+            row.append(get_content(entries[2])) # Credits
 
             #Prerequisite(s)
-            cnt = t_list.count('Prerequisite(s)')
+            cnt = entries.count('Prerequisite(s)')
             if cnt != 0:
-                index = t_list.index('Prerequisite(s)')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Prerequisite(s)')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Exclusion(s)
-            cnt = t_list.count('Exclusion(s)')
+            cnt = entries.count('Exclusion(s)')
             if cnt != 0:
-                index = t_list.index('Exclusion(s)')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Exclusion(s)')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Corequisite(s)
-            cnt = t_list.count('Corequisite(s)')
+            cnt = entries.count('Corequisite(s)')
             if cnt != 0:
-                index = t_list.index('Corequisite(s)')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Corequisite(s)')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Co-list with
-            cnt = t_list.count('Co-list with')
+            cnt = entries.count('Co-list with')
             if cnt != 0:
-                index = t_list.index('Co-list with')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Co-list with')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Mode of Delivery
-            cnt = t_list.count('Mode of Delivery')
+            cnt = entries.count('Mode of Delivery')
             if cnt != 0:
-                index = t_list.index('Mode of Delivery')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Mode of Delivery')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Previous Course Code(s)
-            cnt = t_list.count('Previous Course Code(s)')
+            cnt = entries.count('Previous Course Code(s)')
             if cnt != 0:
-                index = t_list.index('Previous Course Code(s)')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Previous Course Code(s)')
+                row.append(get_content(entries[index + 1]))
             else:
                 row.append('')
 
             #Alternate code(s)
-            cnt = t_list.count('Alternate code(s)')
+            cnt = entries.count('Alternate code(s)')
             if cnt != 0:
-                index = t_list.index('Alternate code(s)')
-                temp = t_list[index + 1]
-                temp = temp.strip('\xa0')
-                row.append(temp)
+                index = entries.index('Alternate code(s)')
+                row.append(get_content(entries[index + 1]))                
             else:
                 row.append('')
 
             #Description
-            cnt = t_list.count('Description')
+            cnt = entries.count('Description')
             if cnt != 0:
-                index = t_list.index('Description')
-                temp_list = t_list[index + 1]
-                temp_list = temp_list.strip('\xa0')
-                temp = temp_list
+                index = entries.index('Description')
+                #print("BEF : " + entries[index + 1])
+                #print()
+                temp = get_content(entries[index + 1])
+                row.append(temp)
+                #print("DESC : " + temp)
+                #print() 
                 #temp_list = temp_list.split(".")
                 #temp = temp_list[0]
+                """
                 temp = temp.replace("\"","")
                 temp = temp.replace("“","")
                 temp = temp.replace("”","")
@@ -175,7 +184,7 @@ def main():
                 temp = temp.replace("’","")
                 temp = temp.replace("‐","")
                 #temp = temp + "......(more on ustspace)" 
-                row.append(temp)
+                """
             else:
                 row.append('')
 
